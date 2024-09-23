65e2ee4e2b473537b7a9d9a9fcbbc96b04eb62a8
PhilippeMitch <philippemitch91@gmail.com>
2024-09-23 18:53:37 +0700

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "" {
   network_interface_ids = [azurerm_network_interface.nic.id]
   admin_ssh_key {
     username   = "adminuser"
-    public_key = ""
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFhWEFhfjd4A/nI3q12synLE0zp85YnwPmjdhhT0N31HEtSzc1mJgtACwZ8KEMD2mekBz9wuUZJEoqVjDUmBr2A3+p7bbdbHMuxd998oimbOBrxaClEgKsWtwn21JG19ZS59db0KeWyU1eUJWErGsXRXepOFn6YWdFgDtFgCLqKtJW5oI1/5ftxMTTRZBGX6CAX6z2/jZRtDtlSBRT+Gscxb0gHSq1JmAuBnw4ui7kJkebcPUheZQsog6lSOs+ec3KP2p3XY1pU0+ALnisGIK5r57rNOF+LCcXgomVvqG+BV52yq5ElIex9aPgG/tv4eUsPDBOerngcZx67CzLbnsiLMF+I3xw+emJgfggKo6n9RdNwbZm0QbK9XFeznHZbUt13v4pmtvUxmY387jJnmu4S0aCbvowlGhCzAjRHsKfomKjeeiA4seKpTrMlerKcTCPP4SngMCQ6ab422v7cu6AnWwpGfxsbD1FVGaeHrf02TbiR3D8IVWgd+ytIb/9xXc= devopsagent@myLinuxVM"
   }
   os_disk {
     caching           = "ReadWrite"
