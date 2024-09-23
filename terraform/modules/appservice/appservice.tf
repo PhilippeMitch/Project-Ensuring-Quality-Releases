2c9950acd19e923bd55da683aef61101069edaa9
PhilippeMitch <philippemitch91@gmail.com>
2024-09-23 21:12:27 +0700

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -17,6 +17,7 @@ resource "azurerm_windows_web_app" "test" {
   }
 
   site_config {
+    always_on                 = false
     application_stack {
       current_stack  = "dotnet"
       dotnet_version = "v4.0"
