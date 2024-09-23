8d6285e556d881dc55bf625468da92796675d012
PhilippeMitch <philippemitch91@gmail.com>
2024-09-23 20:44:11 +0700

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,7 +2,7 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "F1"
 }
 
@@ -15,7 +15,12 @@ resource "azurerm_linux_web_app" "test" {
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
   }
-  site_config {
-    always_on = false
+
+  site_config {}
+
+application_stack {
+    current_stack = "dotnet"
+    dotnet_version = "v4.0"
   }
+
 }
